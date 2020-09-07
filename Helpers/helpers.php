8c77292d7e99391fe89b7ae66b76e0b4a678ee80
Agus Prasetyo <agusprasetyo1889@gmail.com>
2020-09-07 22:59:52 +0700

    update helpers in dua.php

--- Helpers/helpers.php
+++ Helpers/helpers.php
@@ -1,3 +1,37 @@
 <?php
-   
+   // Soal Nomer 2
+   if (!function_exists('hitungTahun')) {
+      /**
+       * Fungsi untuk menghitung jumlah penduduk pertahun
+       *
+       * @param [type] $penduduk
+       * @param [type] $jumlah_kalkulasi
+       * @return integer
+       */
+      function hitungTahun($penduduk, $jumlah_kalkulasi) : int
+      { 
+         $jumlah_tahun = 0;
+
+         $status = true;
+         while ($status) {
+            $penduduk = $penduduk + ($penduduk * 0.02) + 50;
+            
+            // Mengecek apakah jumlah kalkulasi melebihi penduduk atau tidak
+            // jika melebihi maka jumlah tahun akan ditambahkan 1
+            if ($jumlah_kalkulasi >= $penduduk) {
+               $jumlah_tahun++;
+
+            // Jika kondisi penduduk melebihi jumlah_kalkukasi maka status akan berubah menjadi false
+            // Dan jumlah tahun tetap ditambahkan satu 
+            // Dikarenakan dalam soal terdapat kondisi jika (Penduduk Lebih besar atau sama dengan)
+            // Dalam hal ini code yang dibawah adalah kondisi jika lebih besar
+            } else {
+               $jumlah_tahun++;
+               $status = false;
+            }
+         }
+
+         return $jumlah_tahun;
+      }
+   }
 ?>
